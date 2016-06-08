0017e5f63d5ebd8a574fd9f267d6f31444e6bd37
Alex Garnett <axfelix@gmail.com>
2016-06-08 09:35:04 -0700

    fixes reboots, everything should work oob now

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 `git clone https://github.com/axfelix/xmlps-vagrant` + `cd xmlps-vagrant` + `vagrant up`. runs on localhost:8008.
+
+ 
+
+your first time submitting a `.doc` file may fail -- LibreOffice needs to create a config file for the www-data user. trying to fix this.
--- xmlps.sh
+++ xmlps.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository -y ppa:ondrej/php
 sudo add-apt-repository -y ppa:andrei-pozolotin/maven3
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
-sudo -E apt-get -q -y install bibutils libreoffice ure uno-libs3 python3-uno libreoffice-script-provider-python unoconv python-lxml exiftool pandoc pandoc-citeproc libghc-citeproc-hs-data ruby libxml-twig-perl libxml-writer-string-perl libxml-writer-perl php5.5-xsl php5.5-curl php5.5-cli php5.5-mysql sendmail build-essential mysql-server apache2 php5.5-common php5.5 wget curl zip git maven3
+sudo -E apt-get -q -y install bibutils libreoffice ure uno-libs3 python3-uno libreoffice-script-provider-python unoconv python-lxml exiftool pandoc pandoc-citeproc libghc-citeproc-hs-data ruby openjdk-7-jdk libxml-twig-perl libxml-writer-string-perl libxml-writer-perl php5.5-xsl php5.5-curl php5.5-cli php5.5-mysql php5.5-zip sendmail build-essential mysql-server apache2 php5.5-common php5.5 wget curl zip git maven3
 sudo apt-get upgrade -y php5.5-common
 wget -O crfpp.deb https://www.dropbox.com/s/svgq8xyz7bbouov/crfpp_0.58-raring-ppa0_amd64.deb?dl=0
 wget -O libcrfpp.deb https://www.dropbox.com/s/zuycitdtyxuisfz/libcrfpp-dev_0.58-raring-ppa0_amd64.deb?dl=0
@@ -30,6 +30,8 @@ unzip grobid-parent-0.4.0.zip
 sudo mv grobid-grobid-parent-0.4.0 /opt/grobid
 cd /opt/grobid
 mvn -Dmaven.test.skip=true clean install
+cd grobid-service
+mvn -Dmaven.test.skip=true jetty:run-war&
 cd /var/www/
 sudo git clone https://github.com/pkp/xmlps.git
 sudo rm html/index.html
@@ -58,3 +60,12 @@ sudo rm /etc/apache2/sites-enabled/*
 sudo ln -s /etc/apache2/sites-available/xmlps.conf /etc/apache2/sites-enabled/xmlps.conf
 sudo ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
 sudo service apache2 restart
+sudo chsh -s /bin/bash www-data
+sudo su -c "bash /var/www/html/start_queues.sh" www-data
+grobidcroncommand="cd /opt/grobid/grobid-service && mvn -Dmaven.test.skip=true jetty:run-war&"
+grobidcron="@reboot $grobidcroncommand"
+cat <(fgrep -i -v "$grobidcroncommand" <(crontab -l)) <(echo "$grobidcron") | crontab -
+apachecroncommand="sudo service apache2 restart"
+apachecron="@reboot $apachecroncommand"
+cat <(fgrep -i -v "$apachecroncommand" <(crontab -l)) <(echo "$apachecron") | crontab -
+sudo su -c 'cat <(fgrep -i -v "cd /var/www/html && bash /var/www/html/start_queues.sh" <(crontab -l)) <(echo "@reboot cd /var/www/html && bash /var/www/html/start_queues.sh") | crontab -' www-data
