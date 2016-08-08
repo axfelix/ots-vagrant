a0f13ac6311be9699100369bb280493963e9fad8
Alex Garnett <axfelix@gmail.com>
2016-08-08 13:55:58 -0700

    newer libreoffice

--- xmlps.sh
+++ xmlps.sh
@@ -10,6 +10,7 @@ sudo -E apt-get install -q -y software-properties-common
 sudo add-apt-repository -y ppa:ondrej/php
 sudo add-apt-repository -y ppa:andrei-pozolotin/maven3
 sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo add-apt-repository -y ppa:libreoffice/ppa
 sudo apt-get update
 sudo -E apt-get -q -y install bibutils libreoffice ure uno-libs3 python3-uno libreoffice-script-provider-python unoconv python-lxml exiftool pandoc pandoc-citeproc libghc-citeproc-hs-data ruby openjdk-7-jdk libxml-twig-perl libxml-writer-string-perl libxml-writer-perl php5.6-xsl php5.6-curl php5.6-cli php5.6-mysql php5.6-zip sendmail build-essential mysql-server apache2 php5.6-common php5.6 wget curl zip git maven3
 sudo apt-get upgrade -y php5.6-common
